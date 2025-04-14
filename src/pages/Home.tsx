9ec0c681595c8e489ebdcfe057563d103ac0ec9a
Tchomasek <tchom.asek@centrum.cz>
2025-04-14 15:55:59 +0200

    momentum daily start

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,29 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-type Post = {
-  title: string;
-  postText: string;
-  username: string;
-};
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const [listOfPosts, setListOfPosts] = useState<Post[]>([]);
+  let navigate = useNavigate();
 
-  useEffect(() => {
-    axios
-      .get("https://tom-backend-fa61ad019cb5.herokuapp.com/posts")
-      .then((response) => {
-        setListOfPosts(response.data);
-      });
-  }, []);
+  const days = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+
+  const handleDayClick = (day: string) => {
+    navigate(`/day/${day}`);
+  };
+
   return (
-    <div>
-      {listOfPosts.map((value, key) => {
-        return (
-          <div key={key} className="post">
-            <div className="title">{value.title}</div>
-            <div className="body">{value.postText}</div>
-            <div className="footer">{value.username}</div>
-          </div>
-        );
-      })}
+    <div className="calendar">
+      {days.map((day, index) => (
+        <div className="day" key={index} onClick={() => handleDayClick(day)}>
+          {day}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export type TaskType = {
+  title: string;
+};
+
+export type CommentType = {
+  commentBody: string;
+  postId: string;
+};
